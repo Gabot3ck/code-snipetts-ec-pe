24b1bd75a73713afa66ca35cda06c02a8db798b6
Gabot3ck <jesus.castilloc@outlook.com>
2025-01-17 16:31:15 -0300

    se agrega carrusel galerias con swiperjs

--- script.js
+++ script.js
@@ -30,4 +30,77 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   });
+
+  
+
+
+  const postActionsControllers = document.querySelectorAll(
+    ".post-actions-controller"
+  );
+  
+  postActionsControllers.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const targetId = btn.getAttribute("data-target");
+      const postActionsContent = document.getElementById(targetId);
+  
+      if (postActionsContent) {
+        const isVisible = postActionsContent.getAttribute("data-visible");
+  
+        if (isVisible === "false") {
+          postActionsContent.setAttribute("data-visible", "true");
+          postActionsContent.setAttribute("aria-hidden", "false");
+          btn.setAttribute("aria-expanded", "true");
+        } else {
+          postActionsContent.setAttribute("data-visible", "false");
+          postActionsContent.setAttribute("aria-hidden", "true");
+          btn.setAttribute("aria-expanded", "false");
+        }
+      }
+    });
+  });
+  
+  function handleClickOutside(event) {
+    postActionsControllers.forEach((btn) => {
+      const targetId = btn.getAttribute("data-target");
+      const postActionsContent = document.getElementById(targetId);
+  
+      if (postActionsContent && postActionsContent.getAttribute("data-visible") === "true") {
+        if (!postActionsContent.contains(event.target) && event.target !== btn) {
+          postActionsContent.setAttribute("data-visible", "false");
+          postActionsContent.setAttribute("aria-hidden", "true");
+          btn.setAttribute("aria-expanded", "false");
+        }
+      }
+    });
+  }
+  
+  // Swiper
+  var swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    speed: 400,
+    mousewheel: {
+      invert: false,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      900: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  }); 
 });
