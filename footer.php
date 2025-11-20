8046adf3518bdb4d1f5a408b2d890a2a862d8474
gabot3ck <jesus.castilloc@outlook.com>
2025-11-20 17:39:36 -0300

    actualizacion estilos de slogan scanavini footer

--- footer.php
+++ footer.php
@@ -40,7 +40,10 @@ do_action('blocksy:content:bottom');
 			</div>
 			<div class="wrapper-body-contact-for-customer" >
 				<p class="title-contact-for-customer" >Llámanos</p>
-				<p>Atención al cliente <br> <a href="tel:+5112045444">(+51) 1 204 5444</a></p>
+				<p>Atención al cliente <br> 
+					<a href="https://wa.me/51983487908">(+51) 983 487 908</a> <br>
+					<a href="tel:+5112045444">(+51) 1 204 5444</a>
+				</p>
 			</div>
 			</div>
 
@@ -74,28 +77,27 @@ do_action('blocksy:content:bottom');
 
 		<div class="footer__top__marketing">
 			<div>
-				<h6 class="title" >Síguenos en:</h6>
+			<h6 class="title" >Síguenos en:</h6>
 
-				<div class="d-flex gap-2 justify-content-around justify-content-xs-center">
-					<a href="https://www.tiktok.com/@scanavini.peru" target="_blank" rel="noopener">
-						<i class="bi bi-tiktok"></i>
-						<span class="visually-hidden">TikTok</span>
-					</a>
-					<a href="https://www.instagram.com/scanavini_peru" target="_blank" rel="noopener">
-						<i class="bi bi-instagram"></i>
-					</a>
-					<a href="https://www.facebook.com/ScanaviniPeru" target="_blank" rel="noopener">
-						<i class="bi bi-facebook"></i>
-					</a>
-					<a href="https://www.linkedin.com/company/scanavini-comercial-sac" target="_blank" rel="noopener">
-						<i class="bi bi-linkedin"></i>
-					</a>
-				</div>
+			<div class="d-flex gap-2 justify-content-around justify-content-xs-center">
+				<a href="https://www.tiktok.com/@scanavini.peru" aria-label="Síguenos en TikTok - Scanavini Perú" target="_blank">
+				  <i class="bi bi-tiktok"></i>
+				</a>
+				<a href="https://www.instagram.com/scanavini_peru" aria-label="Síguenos en Instagram - Scanavini Perú" target="_blank">
+				  <i class="bi bi-instagram"></i>
+				</a>
+				<a href="https://www.facebook.com/ScanaviniPeru" aria-label="Visita nuestra página de Facebook - Scanavini Perú" target="_blank">
+				  <i class="bi bi-facebook"></i>
+				</a>
+				<a href="https://www.linkedin.com/company/scanavini-comercial-sac" aria-label="Conéctate con nosotros en LinkedIn - Scanavini" target="_blank">
+				  <i class="bi bi-linkedin"></i>
+				</a>
+			</div>
 			</div>
 
 			<div class="claims-book-container" >
 				<a href="https://reclamaciones.tiendascanavini.pe" target="_blank">
-					<img src="https://www.tiendascanavini.pe/wp-content/uploads/2025/04/libro-de-reclamaciones-virtual.png" alt="libro de reclamaciones scanavini" class="img-fluid">
+					<img src="https://www.tiendascanavini.pe/wp-content/uploads/2025/04/libro-de-reclamaciones-virtual.png" alt="libro de reclamaciones virtual scanavini" class="img-fluid">
 				</a>
 			</div>
 		</div>
@@ -110,7 +112,7 @@ do_action('blocksy:content:bottom');
 			</div>
 			<p>&copy; <?php echo date('Y'); ?> Scanavini Comercial  S.A.C.</p>
 		</div>
-	</footer>
+  	</footer>
 
 </div>
 
